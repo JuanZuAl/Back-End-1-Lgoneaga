d12d3b11fe9adee504fa8f8f37b0e191a4e2c70f
Juan Zuluaga <santiago.sk08@gmail.com>
2026-02-07 14:09:46 -0500

    Varias pruebas

--- Repaso2/src/application/Main.java
+++ Repaso2/src/application/Main.java
@@ -1,7 +1,21 @@
 package application;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("hola Mundo");
+        String name = " Juan";
+        int age = 26;
+        boolean male = true;
+        System.out.println(name + " " + age + " años " + male);
+        int anoActual = 2026;
+        final int ANONACIMINETO = 1999;
+        int currentage = anoActual - ANONACIMINETO;
+        System.out.println(currentage + " " + "Es su edad");
+
+
+        for (int i = 1 ; i <= 10; i++ )
+            System.out.println(i);
     }
 }
