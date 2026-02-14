a065e7c7ce327ccbbbac27e4ca9190933936c976
Juan Alberto Zuluaga Ramirez <webdesigner448@gmail.com>
2026-02-14 12:04:29 -0500

    Difinición de variables y metodos

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,25 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Main {
+    final static int CURRENT_YEAR = 2026;
     public static void main(String[] args){
-        System.out.println("Hello and welcome to my new project");
+        final String GREETING_MESSAGE = "Saludo estudiantes de Java"
+
+        String [] namesStudents = {"Juan", "Maria", "Pedro", "Ana", "luis"};
+        int [] birthYear = {2000, 1998, 2002, 1995, 2001};
+
+        list<int[]> agesStudentlist = new ArrayList<>();
+        showMessage(GREETING_MESSAGE);
     }
+    static void showMessage(String message) {
+        System.out.println(message);
+    }
+    static int calculateAge(int  birthYear){
+        return CURRENT_YEAR - birthYear;
+    }
+
 
 
 }
